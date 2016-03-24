ed2cec303618df4951428c21e5ee22b6514228c7
Geraint Jones <geraint-jones91@hotmail.co.uk>
2016-03-24 14:59:56 +0000

    ImportDetails.py - added classes for different types of scope in a VbScript file

--- VBScriptLibraryUtil/ImportDetails.py
+++ VBScriptLibraryUtil/ImportDetails.py
@@ -3,6 +3,8 @@ import os
 import codecs
 #import time
 
+VBSCRIPT_VAR_NAME_PATTERN = '\\b[a-zA-Z]{1}[a-zA-Z0-9_]{,254}\\b'
+
 class FileNotFoundException(Exception):
 	def __init__(self,*args,**kwargs):
 		Exception.__init__(self,*args,**kwargs)
@@ -15,16 +17,142 @@ class IncorrectFileExtensionException(Exception):
 	def __init__(self,*args,**kwargs):
 		Exception.__init__(self,*args,**kwargs)
 
-def getLibraryContents(path, removeComments=True, removeWhiteSpaces=True):
+# used for all scopes apart from the global scope
+class VBSBlockScope:
+	SCOPE_MODIFIERS_PATTERN = '(\\bpublic\\b|\\bprivate\\b)?'
+	startPattern = None
+	endPattern = None
+
+	def __init__(self):
+		self.content = ''
+
+	def appendContent(text):
+		self.content.append(text)
+
+class VBSBlockScopeClass(VBSBlockScope):
+	# setup up the regexp pattern static class variables
+	VBSBlockScope.startPattern = ( '%s\\s*\\bClass\\b\\s+%s' % \
+		(VBSBlockScope.SCOPE_MODIFIERS_PATTERN, VBSCRIPT_VAR_NAME_PATTERN) )
+	VBSBlockScope.endPattern = ( '\\bEnd\\b\\s+\\bClass\\b' )
+
+	def __init__(self, scope, name):
+		VBSBlockScope.__init__(self)
+		self.scope = scope
+		self.name = name		
+
+class VBSBlockScopeMethod(VBSBlockScope):
+	PARAMS_TYPE_PATTERN = '(\\bByVal\\b|\\bByRef\\b)?'
+	METHOD_PARAMS_PATTERN = ( '(\\((\\s*(%s)?\\s*(%s)\\s*,)*)?' % \
+		(PARAMS_TYPE_PATTERN, VBSCRIPT_VAR_NAME_PATTERN) )
+
+	def __init__(self, scope, name, params):
+		VBSBlockScope.__init__(self)
+		self.scope = scope
+		self.name = name
+		self.params = params
+
+	@classmethod
+	def setStartPattern(cls, blockIdentifierPattern):
+		cls.startPattern = ( '%s\\s*%s\\s+%s\\s*%s' % (VBSBlockScope.SCOPE_MODIFIERS_PATTERN, \
+			blockIdentifierPattern, VBSCRIPT_VAR_NAME_PATTERN, VBSBlockScopeMethod.METHOD_PARAMS_PATTERN) )
+
+	@classmethod
+	def setEndPattern(cls, blockIdentifierPattern):
+		cls.endPattern = ( '\\bEnd\\b\\s+%s' % blockIdentifierPattern )
+
+class VBSBlockScopeFunction(VBSBlockScopeMethod):
+	def __init__(self, scope, name, params):
+		VBSBlockScopeMethod.__init__(self, scope, name, params)
+
+	# needs to be called before the class pattern parameters are used (could find nice way to run static init block of code)
+	@classmethod
+	def setupPatterns(cls):
+		cls.setStartPattern("\\bFunction\\b")
+		cls.setEndPattern("\\bFunction\\b")
+
+class VBSBlockScopeSub(VBSBlockScopeMethod):
+	def __init__(self, scope, name, params):
+		VBSBlockScopeMethod.__init__(self, scope, name, params)
+
+	# needs to be called before the class pattern parameters are used (could find nice way to run static init block of code)
+	@classmethod
+	def setupPatterns(cls):
+		cls.setStartPattern("\\bSub\\b")
+		cls.setEndPattern("\\bSub\\b")
+
+class VBSBlockScopePropertyGet(VBSBlockScopeMethod):
+	def __init__(self, scope, name, params):
+		VBSBlockScopeMethod.__init__(self, scope, name, params)
+
+	# needs to be called before the class pattern parameters are used (could find nice way to run static init block of code)
+	@classmethod
+	def setupPatterns(cls):
+		cls.setStartPattern("\\bProperty\\b\\s+\\bGet\\b")
+		cls.setEndPattern("\\bProperty\\b")
+
+class VBSBlockScopePropertyLet(VBSBlockScopeMethod):
+	def __init__(self, scope, name, params):
+		VBSBlockScopeMethod.__init__(self, scope, name, params)
+
+	# needs to be called before the class pattern parameters are used (could find nice way to run static init block of code)
+	@classmethod
+	def setupPatterns(cls):
+		cls.setStartPattern("\\bProperty\\b\\s+\\bLet\\b")
+		cls.setEndPattern("\\bProperty\\b")
+
+class VBSBlockScopePropertySet(VBSBlockScopeMethod):
+	def __init__(self, scope, name, params):
+		VBSBlockScopeMethod.__init__(self, scope, name, params)		
+		
+	# needs to be called before the class pattern parameters are used (could find nice way to run static init block of code)
+	@classmethod
+	def setupPatterns(cls):
+		cls.setStartPattern("\\bProperty\\b\\s+\\bSet\\b")
+		cls.setEndPattern("\\bProperty\\b")
+
+# sets up the classes start and end patterns for the different method classes
+VBSBlockScopeFunction.setupPatterns()
+VBSBlockScopeSub.setupPatterns()
+VBSBlockScopePropertyGet.setupPatterns()
+VBSBlockScopePropertyLet.setupPatterns()
+VBSBlockScopePropertySet.setupPatterns()
+
+# returns formatted string for file with comments removed long with leading and trailing whitespaces
+def getLibraryContentsFormatted(path):
 	outputContents = ''
 	with openTryEncodings(path) as f:
 		for line in f:
-			if removeWhiteSpaces:
-				line = line.strip()
+			# removes comments and white spaces
+			formattedLine = removeComments(line).strip(' \t')
 
-			outputContents += line + '\n'
+			# skip lines that are just white spaces and comments
+			if len(formattedLine) == 0:
+				continue
+
+			# to remove line continuation
+			if (formattedLine[-1] == '_'):
+				outputContents += formattedLine
+			else:
+				outputContents += formattedLine + '\n'
 
 	return outputContents
+
+def getLibraryScopeContents(scope):
+	pass
+
+def removeComments(line):
+	inStr = False
+	pos = 0
+	for char in line:
+		if char == '"':
+			inStr = not inStr
+		# exit loop when comment starts
+		elif (not inStr) and (char == "'"):
+			break
+
+		pos += 1
+
+	return line[:pos]
 
 def isVbScriptFile(path):
 	return (os.path.splitext(path)[1].lower() in ('.vbs', '.qfl'))
