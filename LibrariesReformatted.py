3ccae99b1e203b29e860ff0cd61dc2f639b8ed39
Geraint Jones <geraint-jones91@hotmail.co.uk>
2016-03-24 11:09:15 +0000

    LibrariesReformatted.py - started a reformatting of the Libraries.py file

---
+++ LibrariesReformatted.py
@@ -0,0 +1,82 @@
+# assumes that the files that this is being run on contain valid VB syntax
+# also requires the current file to be in a directory called '\testlibrary\'
+
+# WARNINGS: 
+# - possible errors if functions are commented out (generally not allowed for comments)
+
+#import sublime_plugin
+#import sublime
+#import re
+#import os
+#import codecs
+#import time
+
+from VBScriptLibraryUtil import ImportDetails
+
+VBSCRIPT_ALLOW_VAR_NAME_REGEX = '\\b[a-zA-Z]{1}[a-zA-Z0-9_]{,254}\\b'
+VBSCRIPT_VARIABLE_BASIC_REGEX = 'a-zA-Z0-9_'
+FILE_FOLDER_NAME_REGEX = 'a-zA-Z0-9_\\-'
+LIBRARY_PARENT_FOLDER = '\\TestLibrary\\'
+POSSIBLE_SCRIPT_PARENT_FOLDERS = ['\\TestLibrary\\', '\\RegressionControl\\']
+
+class VBSParameterDetails:
+	def __init__(self, paramType, name):
+		# 'ByVal' or 'ByRef'
+		self.paramType = paramType
+		self.name = name
+
+	# just for testing
+	def toString(self):
+		return (self.paramType, self.name)
+
+class VBSVariableDetails:
+	def __init__(self, name, value):
+		self.name = name
+		# what the variable contains
+		# only used if it is an object and has other details inside it like class with methods etc.
+		# set to None otherwise
+		self.value = value
+
+	# just for testing
+	def toString(self):
+		return (self.name, self.value)
+
+class VBSPropertyDetails(VBSVariableDetails):
+	def __init__(self, scope, name, value):
+		# probably should use .super() but differece between Python 2 and 3 plus don't really know how it works
+		VBSVariableDetails.__init__(self, name, value)
+		# 'Private' or 'Public'
+		self.scope = scope
+
+	# just for testing
+	def toString(self):
+		return (self.scope,) + VBSVariableDetails.toString(self)
+
+class VBSMethodDetails:
+	def __init__(self, scope, methType, name, params):
+		# 'Private' or 'Public'
+		self.scope = scope
+		# 'Function', 'Sub', 'Property Let' or 'Property Set'
+		self.methType = methType
+		self.name = name
+		# a list of VBSParameterDetails classes
+		self.params = params
+
+	# just for testing
+	def toString(self):
+		return (self.scope, self.methType, self.name, self.params)
+
+class VBSClassDetails:
+	def __init__(self, properties, methods):
+		# a list of VBSPropertyDetails classes
+		self.properties = properties
+		# a list of VBSMethodDetails classes
+		self.methods = methods
+
+	# just for testing
+	def toString(self):
+		return (self.properties, self.methods)
+
+if __name__ == '__main__':
+	a = VBSPropertyDetails('public', 'IR', 'CD235434')
+	print(a.toString())
